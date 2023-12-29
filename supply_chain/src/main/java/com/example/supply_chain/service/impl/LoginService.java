a7306c0b5ece1669dab16f5b778a48458810c8a0
SGraceGrace <143794400+SGraceGrace@users.noreply.github.com>
2023-12-29 14:31:59 +0530

    jwt

--- supply_chain/src/main/java/com/example/supply_chain/service/impl/LoginService.java
+++ supply_chain/src/main/java/com/example/supply_chain/service/impl/LoginService.java
@@ -16,15 +16,6 @@ public class LoginService {
 		
 	}
 	
-	public String generatePasswordToken(String userName,String password) {
-		Claims claims = Jwts.claims().setSubject(userName);
-		//System.out.println("claims :"+claims);
-		String token = Jwts.builder().setClaims(claims).signWith(SignatureAlgorithm.HS256, "test").compact();
-		// System.out.println("token :"+token);
-		return token;
-		
-	}
-	
 	public String validateToken(String token,String name){
 		// System.out.println("token :"+token);
         if(token.equals(generateToken(name, ""))){
