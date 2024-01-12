b72263c472a03557b74d0fa3e418370466f3a81d
SGraceGrace <143794400+SGraceGrace@users.noreply.github.com>
2024-01-12 11:36:32 +0530

    authentication

---
+++ user_details/src/main/java/com/login/user_details/UserDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.login.user_details;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDetailsApplication.class, args);
+	}
+
+}
